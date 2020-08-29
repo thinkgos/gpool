c184f349a1b7c1c561e721cb8ac1b53d63a26496
jiang <jgb40@qq.com>
2020-08-29 14:49:10 +0800

    Update revive.sh

--- revive.sh
+++ revive.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 revive -config revive.toml  -formatter friendly ./...
-golangci-lint run
+golint ./... | reviewdog -f=golint -diff="git diff master"
+golangci-lint run --out-format=line-number -E goimports -E misspell -E godot -E lll | reviewdog -f=golangci-lint -diff="git diff master"
